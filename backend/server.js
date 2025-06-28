18c4defa3abf66866e431b592cb05b79d3172d87
mattp532 <pmatt.212@gmail.com>
2025-06-28 02:37:55 -0400

    add docker compose and create default route

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,7 @@ const PORT = process.env.BACKEND_PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send("Diddy")
+});
